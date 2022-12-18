e3c36b3b86b59c418aa99fffa918692c480f082b
Maks <nordmax69@gmail.com>
2022-12-19 03:55:10 +0700

    added TV

--- src/main/java/org/example/module3/TVsets.java
+++ src/main/java/org/example/module3/TVsets.java
@@ -1,15 +1,21 @@
 /*
  Дан массив записей: наименование товара, цена, сорт.
  Найти наибольшую цену товаров 1го или 2го сорта среди товаров,
- название которых содержит «высший
+ название которых содержит «LED
  */
 
 package org.example.module3;
 
 public class TVsets {
     public static void main(String[] args) {
-     Integer r = 20;
-     
+
+        Tv tv1 = new Tv("Samsung",50.0,1);
+        Tv tv2 = new Tv("LG LED",55.0,1);
+        Tv tv3 = new Tv("Philips LED",35.0,2);
+        Tv tv4 = new Tv("BBK",24.0,3);
+        Tv tv5 = new Tv("Thomson",27.0,3);
+        Tv tv6 = new Tv("Xiaomi LED",33.0,2);
+
 
 
     }
---
+++ src/main/java/org/example/module3/Tv.java
@@ -0,0 +1,12 @@
+package org.example.module3;
+
+public class Tv {
+    String name;
+    Double price;
+    Integer variaty;
+    public Tv(String name,Double price,Integer variaty){
+        this.name = name;
+        this.price = price;
+        this.variaty = variaty;
+    }
+}
