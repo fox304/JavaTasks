9d5b812f82c2e6cec7bce6ed60af9c492aa222fa
Maks <nordmax69@gmail.com>
2022-12-20 18:38:54 +0700

    added books

---
+++ src/main/java/org/example/module5/Books.java
@@ -0,0 +1,41 @@
+package org.example.module5;
+
+public class Books {
+    String titleBook,author;
+    Double price;
+    Integer year,count;
+
+    public Books(String titleBook, String author, Double price, Integer year, Integer count) {
+        this.titleBook = titleBook;
+        this.author = author;
+        this.price = price;
+        this.year = year;
+        this.count = count;
+    }
+
+    public String getTitleBook() {
+        return titleBook;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+    public Boolean PrimeNumbers(Integer num) {
+        for (int i = 2; i < num/2 + 1; i++) {
+            if (num%i == 0) return false;
+        }
+        return true;
+    }
+}
